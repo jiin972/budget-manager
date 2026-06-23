f3595300e81f6654081b7fa0bb5014b2aff6d73f
jiin972 <jiin972@gmail.com>
2026-06-24 00:48:33 +0900

    chore: update prisma schema and sqlite adapter - add new model for prisma - add new adapter

---
+++ lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@prisma/client/extension";
+import Database from "better-sqlite3";
+
+const sqlite = new Database("./database.db");
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./database.db",
+});
+
+const db = new PrismaClient({ adapter });
+
+export default db;
