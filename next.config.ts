490e30b4249b3a8ece2bb2af6376a6bf58e1099c
jiin972 <jiin972@gmail.com>
2026-06-30 13:17:02 +0900

    fix: move allowedDevOrigins to root in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    //@ts-ignore
-    allowedDevOrigins: ["100.109.28.107"],
-  },
+  //@ts-ignore
+  allowedDevOrigins: ["100.109.28.107"],
 };
 
 export default nextConfig;
