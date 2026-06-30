497e51c206c88c8d9f858149714bf88f37664d55
jiin972 <jiin972@pop-os.taildeb984.ts.net>
2026-06-30 12:48:27 +0900

    feat: add allowedDevOrigins - allowed server ports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {},
+  //@ts-ignore
+  allowedDevOrigins: ["100.109.28.107"],
 };
 
 export default nextConfig;
