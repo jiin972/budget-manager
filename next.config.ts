4b38cf94af40eb3128eee4a8e2cbbd6c9b345fba
jiin972 <jiin972@pop-os.taildeb984.ts.net>
2026-06-30 12:58:08 +0900

    feat: add option as allowedDevOrigins - allowed port mac-mini(server)

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-  experimental: {},
-  //@ts-ignore
-  allowedDevOrigins: ["100.109.28.107"],
-=======
   experimental: {
     //@ts-ignore
     allowedDevOrigins: ["100.109.28.107"],
   },
->>>>>>> 73aee50ed12fe7fab1081eb6be8a76d11d3295ba
 };
 
 export default nextConfig;
