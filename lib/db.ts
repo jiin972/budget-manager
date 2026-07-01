ea41fac05ba1d9f4b7edd18d0bcf1c3bcfd8fe05
jiin972 <jiin972@gmail.com>
2026-07-02 06:41:08 +0900

    fix: prisma DB파일 경로 통합 및 이전버전DB삭제

--- lib/db.ts
+++ lib/db.ts
@@ -2,10 +2,10 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../app/generated/prisma";
 import Database from "better-sqlite3";
 
-const sqlite = new Database("./database.db");
+const sqlite = new Database("./prisma/database.db");
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:./database.db",
+  url: "file:./prisma/database.db",
 });
 
 const db = new PrismaClient({ adapter });
