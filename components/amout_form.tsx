20b8303d383bb97e7479982a8bf397b2a4998354
jiin972 <jiin972@gmail.com>
2026-07-10 07:29:17 +0900

    refactor: move file to new location and update import paths

--- components/amout_form.tsx
+++ components/amout_form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { addExpense, getOllamaData } from "@/app/action";
+import { addExpense, getOllamaData } from "../app/(main)/home/action";
 import { useActionState, useEffect, useState } from "react";
 
 export default function AmountForm() {
