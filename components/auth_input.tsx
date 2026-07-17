38d237444240ba39c42b75b572cd4cbbc0d22beb
jiin972 <jiin972@gmail.com>
2026-07-18 08:02:32 +0900

    feat: adjust errors type and add style for Auth input - add optional with Errors

--- components/auth_input.tsx
+++ components/auth_input.tsx
@@ -1,14 +1,20 @@
 type inputProps = {
   name: string;
-  errors: string[];
+  errors?: string[];
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function AuthInput({ name, errors = [], ...rest }: inputProps) {
   return (
-    <div>
-      <input name={name} {...rest} />
+    <div className="flex flex-col gap-1 w-full">
+      <input
+        name={name}
+        {...rest}
+        className="w-full rounded-lg bg-slate-800 border border-slate-700 px-4 py-2 placeholder:text-gray-500 focus:outline-none focus:border-emerald-500 transition-colors"
+      />
       {errors?.map((error, index) => (
-        <span key={index}>{error}</span>
+        <span key={index} className="text-rose-400 text-sm">
+          {error}
+        </span>
       ))}
     </div>
   );
