660b77d165d80c1aee9c44dd118037876e8f5680
jiin972 <jiin972@gmail.com>
2026-07-18 08:09:22 +0900

    refactor: convert component to client-side for useActionState and add AuthInput component

--- app/(auth)/create-account/page.tsx
+++ app/(auth)/create-account/page.tsx
@@ -1,47 +1,61 @@
+"use client";
+
+import AuthInput from "@/components/auth_input";
+import { useActionState } from "react";
+import { createAccount } from "./action";
+import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
+
 export default function CreateAccount() {
+  const [state, formAction, isPending] = useActionState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6 ">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the Form below to join!</h2>
       </div>
-      <form action={dispatch} className="flex flex-col gap-2 ">
-        <Input
+      <form action={formAction} className="flex flex-col gap-2 ">
+        <AuthInput
           name="username"
           required
           type="text"
           placeholder="Username"
-          errors={state?.fieldErrors.username}
           minLength={3}
-          defaultValue={state?.payload.username.toString() ?? ""}
+          errors={state?.flattenError.username}
+          defaultValue={state?.payload.username?.toString() ?? ""}
         />
-        <Input
+        <AuthInput
           name="email"
           required
           type="email"
           placeholder="Email"
-          errors={state?.fieldErrors.email}
+          errors={state?.flattenError.email}
           defaultValue={state?.payload.email?.toString() ?? ""}
         />
-        <Input
+        <AuthInput
           name="password"
           required
           type="password"
           placeholder="Password"
-          errors={state?.fieldErrors.password}
-          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.flattenError.password}
           defaultValue={state?.payload.password?.toString() ?? ""}
-        />
-        <Input
-          name="confirm_password"
-          required
-          type="Password"
-          placeholder="Confirm password"
-          errors={state?.fieldErrors.confirm_password}
           minLength={PASSWORD_MIN_LENGTH}
-          defaultValue={state?.payload.confirm_password?.toString() ?? ""}
         />
-        <button>완료</button>
+        <AuthInput
+          name="confirmPassword"
+          required
+          type="password"
+          placeholder="Confirm password"
+          errors={state?.flattenError.confirmPassword}
+          defaultValue={state?.payload.confirmPassword?.toString() ?? ""}
+          minLength={PASSWORD_MIN_LENGTH}
+        />
+        <button
+          type="submit"
+          disabled={isPending}
+          className="mt-5 rounded-lg bg-emerald-400 px-4 py-2 hover:bg-emerald-500 text-white fontfont-semibold transition-colors disabled:bg-gray-500 disabled:cousor-not-allowed"
+        >
+          {isPending ? "저장 중..." : "가입완료"}
+        </button>
       </form>
     </div>
   );
