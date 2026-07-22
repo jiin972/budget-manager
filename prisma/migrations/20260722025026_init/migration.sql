320040ceafcd1ff8cdb4a9ffe15e64fd7dde91dc
jiin972 <jiin972@gmail.com>
2026-07-22 11:52:46 +0900

    feat: reset database schema and re-initialize tables

---
+++ prisma/migrations/20260722025026_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Expense` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Expense" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Expense" ("amount", "category", "content", "createdAt", "id") SELECT "amount", "category", "content", "createdAt", "id" FROM "Expense";
+DROP TABLE "Expense";
+ALTER TABLE "new_Expense" RENAME TO "Expense";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
